eb764bd5c5470899ff5d2d1990f49f68f9e269a8
pradeep <pradeepemc3@gmail.com>
2020-08-18 19:31:51 +0530

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/Creature.kt
@@ -0,0 +1,9 @@
+
+package com.raywenderlich.android.creaturemon.model
+
+data class Creature(
+    val attributes: CreatureAttributes = CreatureAttributes(),
+    val hitPoints: Int = 0,
+    val name: String = "",
+    val drawable: Int = 0
+)
