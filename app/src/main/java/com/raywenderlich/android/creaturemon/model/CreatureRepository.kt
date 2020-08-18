8fdf8c8452ff73ab90dfafedd3f859a8e69aea78
pradeep <pradeepemc3@gmail.com>
2020-08-18 20:22:53 +0530

    Creation of creature Repository and db structure

--- app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureRepository.kt
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureRepository.kt
@@ -1,4 +1,10 @@
 
 package com.raywenderlich.android.creaturemon.model
 
-interface CreatureRepository
+import androidx.lifecycle.LiveData
+
+interface CreatureRepository {
+    fun saveCreature(creature: Creature)
+    fun getAllCreatures(): LiveData<List<Creature>>
+    fun clearAllCreatures()
+}
